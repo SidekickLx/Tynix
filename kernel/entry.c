13a7284d3aa56eebaa4170a39a3843ed46ef4944
sidekicklx <lixuezixiang@gmail.com>
2018-02-13 21:53:28 -0800

    daily modification for testing

--- kernel/entry.c
+++ kernel/entry.c
@@ -5,9 +5,13 @@
 int kern_entry()
 {
 	setup_gdt();
+	setup_idt();
 	console_clear();
 	console_prints_color("hello Tynix!!!\n", rc_black, rc_cyan);
 	console_printn_color(32, 16, rc_black, rc_light_blue);
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");	
+	while(1);
 	return 0;
 }
 
