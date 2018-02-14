ecf8e33f0b01c776fef21dce03e5fc8d03bbcc25
sidekicklx <lixuezixiang@gmail.com>
2018-02-13 21:54:12 -0800

    IDT implementation

--- include/boot.h
+++ include/boot.h
@@ -1,3 +1,8 @@
 #include "x86.h"
+#include "idt.h"
 
+// initialize GDT
 void setup_gdt(void);
+
+// initialize IDT
+void setup_idt(void);
--- include/x86.h
+++ include/x86.h
@@ -47,6 +47,44 @@ struct gdt_entry_t {
  * 
  * =========================================================================
  */
+// 中断描述符
+typedef
+struct idt_entry_t {
+    uint16_t base_lo;        // 中断处理函数地址 15 ～ 0 位
+    uint16_t sel;            // 目标代码段描述符选择子
+    uint8_t  always0;        // 置 0 段
+    uint8_t  flags;          // 一些标志，文档有解释
+    uint16_t base_hi;        // 中断处理函数地址 31 ～ 16 位
+}__attribute__((packed)) idt_entry_t;
+
+// IDTR
+typedef
+struct idt_ptr_t {
+    uint16_t limit;     // 限长
+    uint32_t base;      // 基址
+} __attribute__((packed)) idt_ptr_t;
+
+// state protect struct
+typedef
+struct pt_regs_t {
+    uint32_t ds;        // 用于保存用户的数据段描述符
+    uint32_t edi;       // 从 edi 到 eax 由 pusha 指令压入
+    uint32_t esi; 
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    uint32_t int_no;    // 中断号
+    uint32_t err_code;      // 错误代码(有中断错误代码的中断会由CPU压入)
+    uint32_t eip;       // 以下由处理器自动压入
+    uint32_t cs;        
+    uint32_t eflags;
+    uint32_t useresp;
+    uint32_t ss;
+} pt_regs;
+
 
 
 
