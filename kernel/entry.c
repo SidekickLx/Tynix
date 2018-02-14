cc2134d0ad94210d96c7404ce178e224227d4b47
sidekicklx <lixuezixiang@gmail.com>
2018-02-13 19:27:34 -0800

    dayly test mod

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,10 +1,13 @@
 #include "types.h"
 #include "console.h"
+#include "boot.h"
 
 int kern_entry()
 {
+	setup_gdt();
 	console_clear();
-	console_prints_color("hello Tynix!!!\n", rc_black, rc_light_brown);
+	console_prints_color("hello Tynix!!!\n", rc_black, rc_cyan);
+	console_printn_color(32, 16, rc_black, rc_light_blue);
 	return 0;
 }
 
