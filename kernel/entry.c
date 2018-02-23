d7cec98a2cfa5995b353f7e66b93e95465e7b67d
sidekicklx <lixuezixiang@gmail.com>
2018-02-22 17:44:10 -0800

    some daily testing shit

--- kernel/entry.c
+++ kernel/entry.c
@@ -7,11 +7,9 @@ int kern_entry()
 	setup_gdt();
 	setup_idt();
 	console_clear();
-	console_prints_color("hello Tynix!!!\n", rc_black, rc_cyan);
-	console_printn_color(32, 16, rc_black, rc_light_blue);
+	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 	asm volatile ("int $0x3");
-	asm volatile ("int $0x4");	
-	while(1);
+	asm volatile ("int $0x4");
 	return 0;
 }
 
