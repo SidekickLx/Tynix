290f6fb06ff7ece448933f4677267a7b89582473
sidekicklx <lixuezixiang@gmail.com>
2018-02-24 00:41:33 -0800

    timer test

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,17 +1,15 @@
 #include "types.h"
 #include "console.h"
 #include "boot.h"
-#include "kbd.h"
 
-int kern_entry()
-{
+int kern_entry() {
 	setup_gdt();
 	setup_idt();
-	console_clear();
+	init_timer(1000);
 	init_kbd();
+
+	console_clear();
 	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
-	asm volatile ("int $0x3");
-	asm volatile ("int $0x4");
 	asm volatile ("sti"); 
 	return 0;
 }
