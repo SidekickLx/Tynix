2721174cbcc40fe5c6f15dd2cda260af22099e4d
sidekicklx <lixuezixiang@gmail.com>
2018-02-23 17:23:46 -0800

    testing shit for kbd IRQ

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,15 +1,18 @@
 #include "types.h"
 #include "console.h"
 #include "boot.h"
+#include "kbd.h"
 
 int kern_entry()
 {
 	setup_gdt();
 	setup_idt();
 	console_clear();
+	init_kbd();
 	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 	asm volatile ("int $0x3");
 	asm volatile ("int $0x4");
+	asm volatile ("sti"); 
 	return 0;
 }
 
