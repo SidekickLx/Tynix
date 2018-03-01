2d761bfd50b9d1209630f11ca6a8576a056beda9
sidekicklx <lixuezixiang@gmail.com>
2018-02-28 22:46:40 -0800

    outb_p & inb_p macro

--- include/x86.h
+++ include/x86.h
@@ -127,6 +127,21 @@ static inline uint16_t gs(void) {
 	return seg;
 }
 
+#define outb_p(value,port) \
+__asm__ ("outb %%al,%%dx\n" \
+		"\tjmp 1f\n" \
+		"1:\tjmp 1f\n" \
+		"1:"::"a" (value),"d" (port))
+
+#define inb_p(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al\n" \
+	"\tjmp 1f\n" \
+	"1:\tjmp 1f\n" \
+	"1:":"=a" (_v):"d" (port)); \
+_v; \
+})
+
 
 
 #endif //INCLUDE_X86_H
