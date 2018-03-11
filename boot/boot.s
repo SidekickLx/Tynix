dc35237eebf9e26587f090fbe6a53c5325160e93
sidekicklx <lixuezixiang@gmail.com>
2018-03-11 03:27:38 -0700

    back to use multiboot1 instead of multiboot2

--- boot/boot.s
+++ boot/boot.s
@@ -6,8 +6,7 @@
 ;
 ; ----------------------------------------------------------------
 
-;MBOOT_HEADER_MAGIC 	equ 	0x1BADB002 	; Multiboot 魔数，由规范决定的
-MBOOT_HEADER_MAGIC 	equ 	0x1BADB002
+MBOOT_HEADER_MAGIC 	equ 	0x1BADB002 	; Multiboot 魔数，由规范决定的
 MBOOT_PAGE_ALIGN 	equ 	1 << 0    	; 0 号位表示所有的引导模块将按页(4KB)边界对齐
 MBOOT_MEM_INFO 		equ 	1 << 1    	; 1 号位通过 Multiboot 信息结构的 mem_* 域包括可用内存的信息
 						; (告诉GRUB把内存空间的信息包含在Multiboot信息结构中)
@@ -40,9 +39,9 @@ section .text 	; 代码段从这里开始
 
 ; 在代码段的起始位置设置符合 Multiboot 规范的标记
 
-;dd MBOOT_HEADER_MAGIC 	; GRUB 会通过这个魔数判断该映像是否支持
-;dd MBOOT_HEADER_FLAGS   ; GRUB 的一些加载时选项，其详细注释在定义处
-;dd MBOOT_CHECKSUM       ; 检测数值，其含义在定义处
+dd MBOOT_HEADER_MAGIC 	; GRUB 会通过这个魔数判断该映像是否支持
+dd MBOOT_HEADER_FLAGS   ; GRUB 的一些加载时选项，其详细注释在定义处
+dd MBOOT_CHECKSUM       ; 检测数值，其含义在定义处
 
 [GLOBAL start] 		; 内核代码入口，此处提供该声明给 ld 链接器
 [GLOBAL glb_mboot_ptr] 	; 全局的 struct multiboot * 变量
